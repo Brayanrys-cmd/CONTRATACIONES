6b6657102199ed35b506ec0bf66b2e81df87e585
Brayanrys-cmd <brayanreyes@habi.co>
2024-10-18 11:14:28 -0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ def transformar_datos(df):
     df['FECHA DE RETIRO'].fillna(fecha_hoy, inplace=True)
 
     # Cálculo de nuevas características
-    df['CVR'] = df['Cantidad de Transacciones'] / df['Meta']
+    df['CVR'] = df['Cantidad de Transacciones'] / df['Meta'].replace(0, np.nan)
     df['CVR'] = df['CVR'].fillna(0)
     df['Salario_USD'] = np.where(df['PAIS'] == 'COLOMBIA', df['SALARIO_REFERENTE'] / 4000, np.nan)
     df['diferencia_dias'] = (df['FECHA DE RETIRO'] - df['FECHA DE INGRESO']).dt.days
@@ -96,7 +96,7 @@ def preparar_datos_modelo(df):
     # Selección de columnas relevantes
     columns_to_keep = [
         'HIJOS', 'GENERO', 'Fuente de Reclutamiento', 'Tipo de Contacto','SECTOR',
-        'ESCOLARIDAD_Numerica', 'EDAD', 'CVR_cluster','TIEMPO ESTUDIO','EXPERIENCIA'
+        'ESCOLARIDAD_Numerica', 'EDAD', 'CVR_cluster','TIEMPO ESTUDIO','EXPERIENCIA','LIDER'
     ]
     df = df[columns_to_keep]
 
